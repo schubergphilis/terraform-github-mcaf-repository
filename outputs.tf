4e6024ce9ef9b98628a7c47435bbaa8a4738a88f
Thulasiraj Komminar <>
2020-02-07 13:22:13 +0100

    moified output value based on PR

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "full_name" {
-  value       = var.name != null ? github_repository.default[0].full_name : null
+  value       = github_repository.default[0].full_name
   description = "A string of the form 'orgname/reponame'"
 }

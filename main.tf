20f5d7b094a1d99ab0f70ae532dbebd9c89095da
Marwin Baumann <mbaumann@schubergphilis.com>
2020-10-13 16:32:39 +0200

    change repository_id back to using data source

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ locals {
   ])
 }
 
+data "github_repository" "default" {
+  name = var.create_repository ? github_repository.default.0.name : var.name
+}
+
 resource "github_repository" "default" {
   count                  = var.create_repository ? 1 : 0
   name                   = var.name
@@ -62,7 +66,7 @@ resource "github_branch_protection" "default" {
   enforce_admins    = local.protection[count.index].enforce_admins
   pattern           = local.protection[count.index].branch
   push_restrictions = local.protection[count.index].push_restrictions
-  repository_id     = github_repository.default[0].node_id
+  repository_id     = data.github_repository.default.node_id
 
   dynamic required_pull_request_reviews {
     for_each = local.protection[count.index].required_reviews != null ? { create : true } : {}

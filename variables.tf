16f96b596c06baffffdd465571cf3a189b26f004
Dennis Brouwer <dbrouwer@schubergphilis.com>
2021-09-24 09:34:23 +0200

    Style fix

--- variables.tf
+++ variables.tf
@@ -135,7 +135,7 @@ variable "template_repository" {
     owner      = string
     repository = string
   })
-  default = null
+  default     = null
   description = "The settings of the template repostitory to use on creation"
 }
 
